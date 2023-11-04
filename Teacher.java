fe69b7e9d6888c71d002d8db418f0f901bc640a5
kueth123 <147624765+kueth123@users.noreply.github.com>
2023-11-04 21:53:10 +0300

    Update Teacher.java
    all the changes made

--- Teacher.java
+++ Teacher.java
@@ -1,11 +1,5 @@
 public class Teacher extends Person {
     private String specialization;
-
-    public Teacher(int age, String specialization) {
-        super(age);
-        this.specialization = specialization;
-    }
-
     public Teacher(int age, boolean parentPermission, String name, String specialization) {
         super(age, parentPermission, name);
         this.specialization = specialization;
