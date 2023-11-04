318f5b5375577de07fa522569cd5450e508472c3
kueth123 <147624765+kueth123@users.noreply.github.com>
2023-11-04 21:50:51 +0300

    Update Student.java
    changes made

--- Student.java
+++ Student.java
@@ -1,11 +1,6 @@
 public class Student extends Person {
     private String classroom;
 
-    public Student(int age, String classroom) {
-        super(age);
-        this.classroom = classroom;
-    }
-
     public Student(int age, boolean parentPermission, String name, String classroom) {
         super(age, parentPermission, name);
         this.classroom = classroom;
